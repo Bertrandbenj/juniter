2028108932e5662a2c99c5061da2e50b03e31c05
bertrandbenj <bertrandbenj@gmail.com>
2018-07-31 23:52:16 +0200

    cleanup

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
---DROP DATABASE testdb;
-
---CREATE DATABASE testdb
---  WITH OWNER = testuser
---       ENCODING = 'UTF8'
---       TABLESPACE = pg_default
---       LC_COLLATE = 'en_US.UTF-8'
---       LC_CTYPE = 'en_US.UTF-8'
---       CONNECTION LIMIT = -1;
